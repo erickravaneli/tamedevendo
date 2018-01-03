83997d9bb26fcae0565a367be80f6904b0e3bbb2
erickravaneli <erickravaneli@gmail.com>
2018-01-03 03:40:25 -0200

    added home bottom nav

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-<div id="app">
-  <header>
-    <span>Vue.js PWA</span>
-  </header>
-  <main>
-    <img src="./assets/logo.png" alt="Vue.js PWA">
-    <router-view></router-view>
-  </main>
-</div>
+<v-app id="app">
+  <v-toolbar app></v-toolbar>
+  <v-content>
+    <v-container class="pa-0">
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+</v-app>
 </template>
 
 <script>
@@ -15,40 +14,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <div class="headline text-xs-center pa-5">Active: {{ selectedTab }}</div>
+  <v-bottom-nav absolute :value="true" :active.sync="selectedTab" color="transparent">
+    <v-btn flat color="teal" value="contacts">
+      <span>Contatos</span>
+      <v-icon>people</v-icon>
+    </v-btn>
+    <v-btn disabled flat color="teal" value="dashboard">
+      <span>Detalhes</span>
+      <v-icon>description</v-icon>
+    </v-btn>
+    <v-btn disabled flat color="teal" value="user">
+      <span>Usuário</span>
+      <v-icon>person</v-icon>
+    </v-btn>
+  </v-bottom-nav>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      selectedTab: 'contacts'
+    }
+  }
+}
+</script>
